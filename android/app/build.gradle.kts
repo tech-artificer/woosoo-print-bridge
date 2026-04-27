8996fcd89bdb830a930c72c30ab68806276712f3
Ryan Pastoriza <ryan.pastoriza@dataelites.io>
2026-04-27 20:53:26 +0800

    Set print bridge Android package identity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.woosoo_relay_device"
+    namespace = "com.woosoo.printbridge"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.woosoo_relay_device"
+        applicationId = "com.woosoo.printbridge"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
